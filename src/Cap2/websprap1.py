b428fc37e4eb4b04c13570b5ce7d4c1aafa6e76d
BraulioUP <22050149@alumno.utmetropolitana.edu.mx>
2024-02-08 18:00:04 -0600

    Corrección de importación en websprap1.py

--- src/Cap2/websprap1.py
+++ src/Cap2/websprap1.py
@@ -28,8 +28,7 @@ driver.quit()
 #Para Chrome:
 
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+w
 from selenium.webdriver.common.by import By
 import time
 
