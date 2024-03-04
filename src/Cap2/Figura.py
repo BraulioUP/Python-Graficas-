09de1ad9757ab3641442cdb92e08a08f5857c7df
BraulioUP <22050149@alumno.utmetropolitana.edu.mx>
2024-03-03 20:12:58 -0600

    Agrega importación de pandas y actualiza la función calcula_estadisticas

--- src/Cap2/Figura.py
+++ src/Cap2/Figura.py
@@ -1,11 +1,12 @@
 import matplotlib.pyplot as plt
+import pandas as pd
 
 ruta_datos= "data/Cap8/parosexoedadprov.xls"
 
 def calcula_estadisticas(ciudad, ruta, mujeres=True):
-   # Aquí debes definir la lógica de tu función
-   
-   return ([2435.0, 3744.0, 12567.0, 13774.0], ['MENORES DE 25 AÑOS', 'DE 25 A 29 AÑOS','DE 30 A 44 AÑOS','MAYORES DE 45 AÑOS'])
+    # Aquí debes definir la lógica de tu función
+    return ([2435.0, 3744.0, 12567.0, 13774.0], ['MENORES DE 25 AÑOS', 'DE 25 A 29 AÑOS','DE 30 A 44 AÑOS','MAYORES DE 45 AÑOS'])
+    
 
 def paro_edades_tarta(ciudad, ruta, mujeres=True):
    vals, etqs = calcula_estadisticas(ciudad, ruta, mujeres)
